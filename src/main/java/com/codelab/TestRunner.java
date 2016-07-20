789813992f94371f3c570226b77b288ede93f1fa
meghasobhani <meghasob@gmail.com>
2016-07-19 20:02:07 -0700

    InitialCommit
    Initial Commit

--- src/main/java/com/codelab/TestRunner.java
+++ src/main/java/com/codelab/TestRunner.java
@@ -2,7 +2,7 @@ package com.codelab;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
-import junit.runner.Version;
+
 public class TestRunner {
 	 public static void main(String[] args) {
 	      Result result = JUnitCore.runClasses(SuiteJunit.class);
@@ -10,8 +10,5 @@ public class TestRunner {
 	         System.out.println(failure.toString());
 	      }
 	      System.out.println(result.wasSuccessful());
-	      
-
-
 	   }
 }
